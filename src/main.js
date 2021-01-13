552431c69dc1c0d3e75c040f03a191812f2ce7e8
Vitalii Koretskyi <vitalii@sucasuc.com>
2021-01-13 18:01:37 +0100

    Add dev enviropment

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 import VueWnTable from "./components";
 
-const VueWnTablePlugin = {
-  install(Vue) {
-    Vue.component(VueWnTable.name, VueWnTable);
-  }
-};
-
-// Automatic installation if Vue has been added to the global scope.
-if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(VueWnTablePlugin);
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component(VueWnTable.name, VueWnTable);
 }
 
-export default VueWnTablePlugin;
-export { VueWnTable };
+// Create module definition for Vue.use()
+const plugin = {
+  install
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default VueWnTable;
